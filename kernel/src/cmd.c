dc09203d7ef313cc326905051ad4cf5594c92e38
RIRU <riruoda@gmail.com>
2023-10-03 22:53:33 +0900

    Create print_* functions for debug

--- kernel/src/cmd.c
+++ kernel/src/cmd.c
@@ -9,10 +9,75 @@
 
 #include "memory.h"
 #include "parse.h"
+#include "module.h"
+#include "vector.h"
 
 static void fatal(const char *msg) {
     perror(msg);
     exit(EXIT_FAILURE);
+}
+
+static void print_func(functype_t *func) {
+    printf("args: ");
+
+    VECTOR_FOR_EACH(e, func->rt1, uint8_t) {
+        printf("%#x ", *e);
+    };
+    putchar('\n');
+
+    printf("returns: ");
+    VECTOR_FOR_EACH(e, func->rt2, uint8_t) {
+        printf("%#x ", *e);
+    };
+    putchar('\n');
+}
+
+static void print_typesec(struct section *sec) {
+    puts("[Type Section]");
+
+    VECTOR_FOR_EACH(elem, sec->typeidxes, functype_t) {
+        print_func(elem);
+    };
+}
+
+static void print_funcsec(struct section *sec) {
+    puts("[Function Section]");
+
+    VECTOR_FOR_EACH(elem, sec->typeidxes, uint32_t) {
+        printf("%#x ", *elem);
+    };
+    putchar('\n');
+}
+
+static void print_instr(instr_t *i) {
+    switch(i->op) {
+        case OP_END:
+            puts("end");
+        // todo: add here
+    }
+}
+
+static void print_code(code_t *code) {
+    printf("size: %#x\n", code->size);
+    printf("locals: ");
+
+    func_t *func = &code->func;
+
+    VECTOR_FOR_EACH(locals, func->locals, locals_t) {
+        printf("(%#x, %#x) ", locals->n, locals->type);
+    }
+    putchar('\n');
+
+    for(instr_t *i = func->expr; i; i = i -> next) {
+        print_instr(i);
+    }
+}
+
+static void print_codesec(struct section *sec) {
+    puts("[Code Section]");
+    VECTOR_FOR_EACH(code, sec->codes, code_t) {
+        print_code(code);
+    };
 }
 
 int main(int argc, char *argv[]) {
@@ -39,8 +104,17 @@ int main(int argc, char *argv[]) {
     if(head == MAP_FAILED) fatal("mmap");
     
     struct buffer *buf = new_buffer(head, fsize);
-    parse_module(buf);
+    struct module *mod = parse_module(buf);
 
+    if(mod->known_sections[1])
+        print_typesec(mod->known_sections[1]);
+    
+    if(mod->known_sections[3])
+        print_funcsec(mod->known_sections[3]);
+    
+    if(mod->known_sections[10])
+        print_codesec(mod->known_sections[10]);
+    
     // cleanup
     munmap(head, fsize);
     close(fd);
