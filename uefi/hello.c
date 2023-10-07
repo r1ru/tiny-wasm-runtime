6cedc8ce0a9318093fdeac42ad2bb207277a630e
RIRU <riruoda@gmail.com>
2023-10-07 22:04:23 +0900

    Delete uefi

--- uefi/hello.c
+++
@@ -1,8 +0,0 @@
-#include <efi.h>
-#include <efilib.h>
- 
-EFI_STATUS efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
-    InitializeLib(image, systab);
-    Print(L"Hello, world!\n");
-    return EFI_SUCCESS;
-}
